13b5076d60517d92f5dc8b85ae0dab305dc05297
ryan3235 <31399454+ryan3235@users.noreply.github.com>
2017-11-11 11:13:03 +1100

    Modfication for route file
    #11 Add new routes for map function.

---
+++ watson/firsrwasproject/wasapp/config/routes.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Create routes for your application here.
+"""
+routes = {
+    'index': {'path': '/','options': {'controller': 'wasapp.controllers.Index'}},
+    'testpage':{'path':'/testpage','options': {'controller': 'wasapp.controllers.Testpage'}},
+    'showData':{'path':'/showData','options': {'controller': 'wasapp.controllers.ShowData'}},
+    'login':{'path':'/login','options': {'controller': 'wasapp.controllers.Login'}},
+    'putdata':{'path':'/putdata','options': {'controller': 'wasapp.controllers.Putdata'}},
+    'signup':{'path':'/signUp','options': {'controller': 'wasapp.controllers.Signup'}},
+    'generatemap':{'path':'/generatemap', 'options':{'controller':'wasapp.controllers.Generatemap'}}
+ # 'route_test': {'path': '/resource','accepts': ('GET', 'POST')'options': {'controller': 'wasapp.controllers.Resource'}}
+ #    'MyHome':{'path':'/myData','options': {'controller': 'wasapp.controllers.Mydata'},'defaults': {'format': 'json'}}
+}
+#controllerBpm
